34df414f3572cdc56c3458b1994d018fece13dfe
yamada4649 <ryuki.yamada4649@gmail.com>
2021-06-20 17:46:46 +0900

    userブランチからbodyカラムを追加しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_075022) do
+ActiveRecord::Schema.define(version: 2021_06_20_084359) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "body"
   end
 
 end
